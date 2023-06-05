1d38afae1becd863fabf8ddd6a36d70a391e0673
Abhishek <abhishek.chandani@canopusinfosystems.com>
2023-06-05 16:57:30 +0530

    Initial commit with cocoa lib template

---
+++ Example/Pods/Target Support Files/Pods-QRCodeSwiftScanner_Example/Pods-QRCodeSwiftScanner_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QRCodeSwiftScanner_Example : NSObject
+@end
+@implementation PodsDummy_Pods_QRCodeSwiftScanner_Example
+@end
---
+++ Example/Pods/Target Support Files/QRCodeSwiftScanner/QRCodeSwiftScanner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QRCodeSwiftScanner : NSObject
+@end
+@implementation PodsDummy_QRCodeSwiftScanner
+@end
